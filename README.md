308c58807e5b1256dbad8d0dcef0a3f54ad9c92d
Yuanqing <yl11249@nyu.edu>
2023-09-23 20:18:47 -0400

    Yayoi Kusama - yellow artwork

--- README.md
+++ README.md
@@ -44,11 +44,12 @@ Link:
 
 
 Image with url:  
-![image description](https://dm-gy-6063-2023f-d.github.io/assets/homework/02/clark-espaco-modulado-00.jpg)
+![Yayoi Kusama Work-A](https://dm-gy-6063-2023f-d.github.io/assets/homework/03/kusama-00.jpg)
 
 
 Image on repo:  
-![image description](./file-name.jpg)
+![Process 1](./HW03A-01.jpg)  
+![Process 2](./HW03A-02.jpg)
 
 
 To start a new line, add two spaces at the end of a line, like this:  
--- sketch.js
+++ sketch.js
@@ -1,6 +1,39 @@
+let cDiam = 30;
+let xSpacing = 100;
+let ySpacing = 100;
+
+let vDiam = 10;
+let yPos2 = yPos;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
+  background(229, 208, 50);
+  noStroke();
+
+  for (let xPos = 0; xPos <= width ; xPos += xSpacing){
+    for (let xPos2 = xPos + 50; xPos2 <= width; xPos2 += xSpacing) {
+      for (let yPos = 0; yPos <= height; yPos += ySpacing ) {
+        for (let yPos2 = yPos; yPos2 <= height; yPos2 += ySpacing){
+            fill (0)
+            ellipse (xPos, yPos, cDiam);
+            ellipse (xPos2, yPos2, vDiam)
+        }
+      }
+    }
+  }
+
+  for (let xPos = 50; xPos <= width ; xPos += xSpacing){
+    for (let xPos2 = xPos + 50; xPos2 <= width; xPos2 += xSpacing) {
+      for (let yPos = 50; yPos <= height; yPos += ySpacing ) {
+        for (let yPos2 = yPos; yPos2 <= height; yPos2 += ySpacing){
+            fill (0)
+            ellipse (xPos, yPos, cDiam);
+            ellipse (xPos2, yPos2, vDiam)
+        }
+      }
+    }
+  }
 }
