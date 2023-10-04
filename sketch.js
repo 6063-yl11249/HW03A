f7bcd47ddc68618dde9a746617aab38718285c5f
Yuanqing <yl11249@nyu.edu>
2023-10-04 10:53:44 -0400

    Simplify for loops

--- sketch.js
+++ sketch.js
@@ -13,27 +13,19 @@ function draw() {
   background(229, 208, 50);
   noStroke();
 
-  for (let xPos = 0; xPos <= width ; xPos += xSpacing){
-    for (let xPos2 = xPos + 50; xPos2 <= width; xPos2 += xSpacing) {
-      for (let yPos = 0; yPos <= height; yPos += ySpacing ) {
-        for (let yPos2 = yPos; yPos2 <= height; yPos2 += ySpacing){
-            fill (0)
-            ellipse (xPos, yPos, cDiam);
-            ellipse (xPos2, yPos2, vDiam)
-        }
-      }
+  for (let xPos = 0; xPos <= width; xPos += xSpacing) {
+    for (let yPos = 0; yPos <= height; yPos += ySpacing) {
+      fill(0);
+      ellipse(xPos, yPos, cDiam);
+      ellipse(xPos + 50, yPos, vDiam);
     }
   }
 
-  for (let xPos = 50; xPos <= width ; xPos += xSpacing){
-    for (let xPos2 = xPos + 50; xPos2 <= width; xPos2 += xSpacing) {
-      for (let yPos = 50; yPos <= height; yPos += ySpacing ) {
-        for (let yPos2 = yPos; yPos2 <= height; yPos2 += ySpacing){
-            fill (0)
-            ellipse (xPos, yPos, cDiam);
-            ellipse (xPos2, yPos2, vDiam)
-        }
-      }
+  for (let xPos = 50; xPos <= width; xPos += xSpacing) {
+    for (let yPos = 50; yPos <= height; yPos += ySpacing) {
+      fill(0);
+      ellipse(xPos, yPos, cDiam);
+      ellipse(xPos + 50, yPos, vDiam);
     }
   }
 }
